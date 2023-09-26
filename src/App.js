056d66661eeccbfc4741128afbaaeb16a8e63cb7
FarhadH1423 <16303038@iubat.edu>
2023-09-26 23:24:28 +0600

    extension pack added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Country></Country>
     </div>
   );
 }
 
-function LoadCountries(){
-  const [countries, setCountries] =useState([]);
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=> setCountries(data))
 
-  } , [])
-  return(
-    <div>
-      <h3>Available Countries: {countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common} population= {country.population}></Country>)
-      }
-    </div>
-  )
-}
 
-function Country(props){
-  return(
-    <div>
-      <p>Name: {props.name}</p>
-      <h4>Population: {props.population}</h4>
-    </div>
-  )
 
-}
+// function LoadCountries(){
+//   const [countries, setCountries] =useState([]);
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=> setCountries(data))
+
+//   } , [])
+//   return(
+//     <div>
+//       <h3>Available Countries: {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population= {country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props){
+//   return(
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <h4>Population: {props.population}</h4>
+//     </div>
+//   )
+
+// }
 
 export default App;
